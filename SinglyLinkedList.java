683eceb7828c21c75dfd604d53b34e095a2a0af7
Quirky0Name <nick.ng.621@gmail.com>
2026-09-20 02:28:06 +0800

    attempt1

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -101,7 +101,51 @@ public class SinglyLinkedList<E extends Comparable<E>> {
 
     // write your codes here
     public void swap(){
+
+        // get ArrayList of all elements
+        List<Node<E>> original = new ArrayList<>();
+        Map elementNode = new TreeMap<>();
+        Node current = head;
+        while (current != null) {
+            original.add(current);
+            elementNode.put(current.getElement(), current);
+            current = current.getNext();
+        }
+
+        List<Map.Entry<E, Node<E>>> entries = new ArrayList<>(elementNode.entrySet());
+// System.out.println(entries);
         
+        int total = entries.size();
+
+        // array to sort new node order
+        Node[] sortedNodes = new Node[total];
+        int left = 0;
+        int right = total - 1;
+        while (left < right) {
+            Node small = entries.get(left).getValue();
+            Node big = entries.get(right).getValue();
+            int smallIndex = original.indexOf(small);
+            int bigIndex = original.indexOf(big);
+
+            sortedNodes[smallIndex] = big;
+            sortedNodes[bigIndex] = small;
+            left++;
+            right--;
+        }
+        if (left == right) { // handle middle
+            Node small = entries.get(left).getValue();
+            int smallIndex = original.indexOf(small);
+            sortedNodes[smallIndex] = small;
+        }
+        
+        // build new links from order
+        head = sortedNodes[0];
+        current = head;
+        for (int i = 1; i < total; i++) {
+            current.setNext(sortedNodes[i]);
+            current = current.getNext();
+        }
+        current.setNext(null);
 
     }
    
